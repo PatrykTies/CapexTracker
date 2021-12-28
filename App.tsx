4d69dc0104eddca16a339fc919ed47eaf90b6a38
Patryk Ties <PatrykTies@users.noreply.github.com>
2021-12-28 18:36:58 +0000

    Dev (#14)
    * add custom fonts theme (#12)
    
    Co-authored-by: Patryk <patryk.ties@lovehomeswap.com>
    
    * Vector icons login (#13)
    
    * make fonts work with vector icons
    
    * add login screen
    
    Co-authored-by: Patryk <patryk.ties@lovehomeswap.com>
    
    Co-authored-by: Patryk <patryk.ties@lovehomeswap.com>

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigator from './src/navigation/MainNavigator';
 import AuthNavigator from './src/navigation/AuthNavigator';
+import {ThemeProvider} from '@shopify/restyle';
+import theme from './src/theme';
 
 const App = () => {
   const isAuthenticated = false;
   return (
-    <NavigationContainer>
-      {isAuthenticated ? <MainNavigator /> : <AuthNavigator />}
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <StatusBar />
+      <NavigationContainer>
+        {isAuthenticated ? <MainNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
--- src/screens/AuthScreens/Login/index.tsx
+++ src/screens/AuthScreens/Login/index.tsx
@@ -1,26 +1,144 @@
-import React from 'react';
-import {View, TextInput, Button} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  ImageBackground,
+  Image,
+  Dimensions,
+  Platform,
+} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+import Button from '../../../components/atoms/Button';
+import LinkButton from '../../../components/atoms/LinkButton';
+import TextField from '../../../components/atoms/TextField';
+import {Card, Box, Text} from '../../../theme';
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email('Keep on typing your email.').required('Required'),
+  password: Yup.string()
+    .min(2, 'Keep on typing. Too Short!')
+    .max(30, 'Too Long!')
+    .required('Required'),
+});
+
+const {width, height: wHeight} = Dimensions.get('window');
 
 const Login = ({navigation}: any) => {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const password = useRef(null);
+  const inputChangeHandler = () => {};
+  const submitHandler = () => {
+    console.log('asdads');
+  };
+  const {handleChange, handleBlur, handleSubmit, values, errors, touched} =
+    useFormik({
+      initialValues: {email: '', password: ''},
+      validationSchema: LoginSchema,
+      onSubmit: v => submitHandler(v),
+    });
 
   return (
-    <View>
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Login" onPress={() => console.log({email, password})} />
-      <Button
-        title="Reset"
-        onPress={() => navigation.navigate('ResetPassword')}
-      />
-    </View>
+    <KeyboardAwareScrollView scrollEnabled={false}>
+      <SafeAreaView
+        height={
+          wHeight - (Platform.OS === 'android' ? StatusBar.currentHeight : 0)
+        }>
+        <ImageBackground
+          source={require('../../../assets/homepage_hero_375.jpg')}
+          style={styles.bgimage}>
+          <Image
+            source={require('../../../assets/makewaves.png')}
+            style={styles.logo}
+          />
+          <Box style={styles.screenContainer}>
+            <Text variant="h1" paddingBottom="xl">
+              Hello <Text variant="h1_deco">make waves</Text>
+            </Text>
+            <Card
+              variant="shadow_md"
+              justifyContent="center"
+              alignItems="center"
+              width={width - 24}
+              paddingBottom="md"
+              backgroundColor="primaryBckgr">
+              <TextField
+                icon="envelope"
+                label="Email"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                error={errors.email}
+                touched={touched.email}
+                autoCompleteType="email"
+                autoCapitalize="none"
+                returnKeyType="next"
+                returnKeyLabel="Next"
+                onSubmitEditing={() => password.current?.focus()}
+              />
+              <TextField
+                ref={password}
+                icon="key"
+                label="Password"
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                error={errors.password}
+                touched={touched.password}
+                autoCompleteType="password"
+                autoCapitalize="none"
+                returnKeyType="go"
+                returnKeyLabel="Finish"
+                secureTextEntry
+                onSubmitEditing={() => handleSubmit()}
+              />
+              <Box
+                flexDirection="row"
+                alignItems="center"
+                width="100%"
+                paddingTop="xl"
+                paddingBottom="lg">
+                <Box flex={1} />
+                <Box flex={1}>
+                  <Button
+                    variant="primary"
+                    label="LOGIN"
+                    onPress={handleSubmit}
+                  />
+                </Box>
+                <Box flex={1} alignItems="center">
+                  <LinkButton
+                    variant="primary"
+                    label="FORGOT?"
+                    onPress={() => navigation.navigate('ResetPassword')}
+                  />
+                </Box>
+              </Box>
+            </Card>
+          </Box>
+        </ImageBackground>
+      </SafeAreaView>
+    </KeyboardAwareScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bgimage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  logo: {
+    width: 60,
+    height: 60,
+    position: 'absolute',
+    top: 24,
+    left: 24,
+    resizeMode: 'contain',
+  },
+});
 
 export default Login;

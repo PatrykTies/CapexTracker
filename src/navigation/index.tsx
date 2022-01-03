3ddabed926252a1a3c55462c86a70a54f9bac45b
Patryk <patryk.ties@lovehomeswap.com>
2022-01-03 22:54:33 +0000

    add login with redux

---
+++ src/navigation/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import LoggedInScreens from './LoggedInScreens';
+import LoggedOutScreens from './LoggedOutScreens';
+
+const AppScreensNavigation = () => {
+  const isAuthenticated = useSelector(
+    (state: any) => state.auth.isAuthenticated,
+  );
+
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <LoggedInScreens /> : <LoggedOutScreens />}
+    </NavigationContainer>
+  );
+};
+
+export default AppScreensNavigation;
